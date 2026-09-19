1508deb8bfd60fb7b9b8cf76c8cb93968c08e533
Adam ghanmi <242857573+Ghanmidev2012@users.noreply.github.com>
2026-09-19 11:31:04 +0100

    Add files via upload

---
+++ app.java
@@ -0,0 +1,24 @@
+
+
+public class app {
+
+    public static void main(String[] args) {
+        
+       
+
+        //variables
+        String name = "adam";
+        int age = 14;
+        String contry = "morocco";
+        String Techstcak = "HTML,CSS,JAVASCRIPT.PYTHON,GO,REACT";
+        String Futurestack = "JAVA,GO";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(contry);
+        System.out.println(Techstcak);
+        System.out.println(Futurestack);
+
+
+    }
+}
